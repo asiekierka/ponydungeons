b530a605249d8d500d2a93d19068511415e28698
Andrea Faulds <ajf@ajf.me>
2013-05-12 19:13:01 +0100

    Use fluttershys and average FPS

--- htdocs/script/test.js
+++ htdocs/script/test.js
@@ -10,39 +10,57 @@
     };
   })();
 
-  var col = new Array("orange","blue","purple","green","pink","white","yellow","gray","red","cyan");
+  var secs, frames = 0;
 
+  function timeSecs () {
+    return (new Date()).getTime() / 1000;
+  }
+
+  function createFluttershy (x, y, z) {
+    var group = new ponydungeons.Canvas.Group(x, y, z), chr;
+
+    chr = new ponydungeons.Canvas.Character('-', 'rgb(253, 181, 203)', -8, 0);
+    group.add(chr);
+    chr = new ponydungeons.Canvas.Character('@', 'rgb(255, 244, 157)', 0, 0);
+    group.add(chr);
+    chr = new ponydungeons.Canvas.Character('-', 'rgb(253, 181, 203)', 8, 0);
+    group.add(chr);
+    chr = new ponydungeons.Canvas.Character('~', 'rgb(253, 181, 203)', 8, 8);
+    group.add(chr);
+    chr = new ponydungeons.Canvas.Character('~', 'rgb(253, 181, 203)', 16, 8);
+    group.add(chr);
+
+    return group;
+  }
+  
   window.onload = function () {
     var cv, g, c1, c2, c3;
     
     cv = new ponydungeons.Canvas(document.body, 640, 480, 'black');
 
     g = new Array();
-    for(var j=0;j<10;j++) {
-      g[j] = new ponydungeons.Canvas.Group(Math.random()*300, Math.random()*300);
+    for(var j=0;j<80;j++) {
+      g[j] = createFluttershy(Math.random()*300, Math.random()*300, 0);
       g[j].scaleX = g[j].scaleY = 3;
       g[j].angle = Math.PI / 3;
       cv.scene.add(g[j]);
-      for(var i=0;i<100;i++) {
-        var c = new ponydungeons.Canvas.Character('@', col[j], (Math.random()-0.5)*100, (Math.random()-0.5)*100);
-        g[j].add(c);
-      }
     }
-    g[0].scaleX = 100;
-    g[0].z = 1;
     cv.scene.sort();
     cv.render();
     
     var animloop = function() {
-      for(var i=0;i<10;i++) {
-        if(i<5) g[i].angle+=6;
+      for(var i=0;i<80;i++) {
+        if(i<40) g[i].angle+=6;
         else g[i].angle-=6;
         g[i].x += ((i%5)+1)/10;
         g[i].y += ((i%5)+1)/10;
       }
       cv.render();
+      frames += 1;
+      console.log('Average FPS since start: ' + (frames/(timeSecs() - secs)));
       requestAnimFrame(animloop);
     };
+    secs = timeSecs();
     requestAnimFrame(animloop);
   };
 }());
